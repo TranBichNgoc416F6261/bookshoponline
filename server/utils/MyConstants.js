0c0ca363e15e1da40bb56f3723d207338cec7200
OooohThisSeemSoInteresting <ngoc.2174802010904@vanlanguni.vn>
2024-07-16 17:12:44 +0700

    client-admin commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster.2jtq03e.mongodb.net',
+    DB_USER: 'tbngoc30112003',
+    DB_PASS: 'F8zVxJjjOe8fJZUl',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'KHONG_CO_KHOA',
+    JWT_EXPIRES: '341123123123123', // in milliseconds
+    EMAIL_USER: 'tbngoc30112003@gmail.com', // gmail service
+    EMAIL_PASS: 'utaxyxgspnhdapbc'
+  };
+  module.exports = MyConstants;
